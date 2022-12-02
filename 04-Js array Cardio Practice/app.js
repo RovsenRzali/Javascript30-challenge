1df34bbd90b1854480b870f633b06750949ae9dc
RovsenRzali <rovsen_204@mail.ru>
2022-12-02 12:08:40 +0400

    finde de in the given text

--- 04-Js array Cardio Practice/app.js
+++ 04-Js array Cardio Practice/app.js
@@ -112,3 +112,20 @@ const oldest = inventors.sort((a,b)=>{
 }) 
 
 console.table(oldest)
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+//Bu kodları, yuxarıdakı linkin açdıqda, brouzerin konsolunda yazıb çalışdırırıq
+
+// const category = document.querySelector('.mw-category')
+// const links = Array.from(category.querySelectorAll('a'))
+// const de = links
+//                  .map(link=>link.textContent)
+//                  .filter(streetName=>streetName.includes('de'))
+/*arrayy' çevirmək üçün aşağıdakı fromadada yaza bilərik
+const links = [category.querySelectorAll('a')] */
+
+
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
