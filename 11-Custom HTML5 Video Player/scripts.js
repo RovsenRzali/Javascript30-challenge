3394f68cd546d9c2edf4804b341a1f90a5fdf218
RovsenRzali <rovsen_204@mail.ru>
2022-12-16 12:27:04 +0400

    Get elemnts

--- 11-Custom HTML5 Video Player/scripts.js
+++ 11-Custom HTML5 Video Player/scripts.js
@@ -1,6 +1,12 @@
 // get our Elements
- const player = document.querySelector('.player')
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
-// Build out functions 
+// Build out functions
 
 // Hook up the events listners
